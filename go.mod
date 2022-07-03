41d8b96915dae9699d1aba33233b748603c10013
sigumaa <me@shiyui.dev>
2022-07-03 14:42:49 +0900

    メッセージを変換する処理を追加しましたわ。

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ogo
 
 go 1.16
+
+require github.com/jiro4989/ojosama v0.7.0
--- main.go
+++ main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"github.com/jiro4989/ojosama"
+	"os"
+)
+
+func main() {
+	args := os.Args[1]
+
+	text, err := ojosama.Convert(args, nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(text)
+}
